e2b8c4caa49ab0e7466b42c0edea62af3938fc01
Aday <abdirizakgulet@gmail.com>
2024-04-14 01:34:58 +0300

    done lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names,occasion){
+    let messages=[];
+    for (let i=0;i<names.length;i++){
+        let message=`Thank you, ${names[i]}, for the wonderful ${ occasion} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+const names=["Guadalupe","Ollie","Aki"];
+const occassion="birthday";
+const messages=writeCards(names,occassion);
+    
+
+function countDown(num){
+    for (let i=num;i>=0;i--){
+        console.log(i);
+    }
+}
+countDown(10);
